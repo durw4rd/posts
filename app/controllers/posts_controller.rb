f3eecb39945cc57c1a5dcaa2643a6fd754a12830
durw4rd <misa.fasa@gmail.com>
2016-09-12 14:38:56 +0200

    user's post page and links to it

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,8 @@ class PostsController < ApplicationController
 
   def user
     @user = User.find( params[:user_id] )
+
+    @posts = Post.where( user: @user ).order( created_at: :desc )
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   root 'posts#index'
 
+  get 'user_posts/:user_id' => 'posts#user', as: :user_posts
+
 end
