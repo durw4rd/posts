040f670ad6cf7cbb9631b9a37bbb1448c8f6d14e
durw4rd <misa.fasa@gmail.com>
2016-09-12 14:18:25 +0200

    show post user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts
-  # 
-  # def handle
-  #   self.email.split('@')[0]
-  # end
+
+  def handle
+    self.email.split('@')[0]
+  end
 end
